4904b9afe023ca80246294580697126757eadf5d
ronngo <ronngomn23@gmail.com>
2020-09-29 11:09:00 +0700

    first commit

---
+++ Book.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocGhiFileXML
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public float Price { get; set; }
+    }
+}
